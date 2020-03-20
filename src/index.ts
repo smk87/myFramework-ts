1423634bdbfec73e597c920bbf20b48207ed0385
Saqlain Mahmud <sqlnmhmdkhanemon@gmail.com>
2020-03-20 22:36:01 +0600

    ADD: User model class.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hi there");
+import { User } from './models/User';
+
+const user = new User({ name: 'myName', age: 20 });
+
+user.set({name:'newName'})
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+
+user.on('change',()=>{
+    console.log('changed!')
+})
+
+console.log(user)
---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+interface UserProps {
+    name?: string;
+    age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+    events: {[key: string]: Callback[]} = {};
+    
+    constructor(private data: UserProps) {}
+
+    get(propName: string): number | string {
+        return this.data[propName];
+    }
+
+    set(update: UserProps): void{
+        Object.assign(this.data,update);
+    }
+
+    on(eventName: string, callback: Callback): void{
+        const handlers = this.events[eventName] || [];
+
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
+}
